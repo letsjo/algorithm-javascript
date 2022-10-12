b90b5f78fc51e67a0079d7516a80f2ac48eceaa9
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-12 13:40:02 +0900

    completed

--- 05 함수/03 한수(1065번)/app.js
+++ 05 함수/03 한수(1065번)/app.js
@@ -6,25 +6,32 @@
 // let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\r\n');
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\r\n');
 
-// let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "utf-8" }).split("\n");
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt", { encoding: "utf-8" })
+  .split("\n")
+  .map(Number);
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n');
 
+// console.log(input[0]);
 
-
-function createNumber(){
-  const arr = Array.from({ length: 9999 }, (_, index) => index+1);
-  for (let n = 1; n < 10000; n++){
-    let tmp = n+(""+n).split('').map(Number).reduce((sum,number)=>sum+=number);
-    while(arr.includes(tmp)){
-      arr.splice(arr.indexOf(tmp),1);
-    }
-  }
-  let answer = "";
-  for (let i = 0; i < arr.length; i++){
-    // console.log(arr[i]);
-    answer += arr[i]+"\n";
-  }
-  console.log(answer);
+if (input[0] < 100) {
+  console.log(input[0]);
+} else if (input[0] == 1000) {
+  console.log(144);
+} else {
+  console.log(checkNumber(input[0]));
 }
 
-createNumber();
+function checkNumber(checkNumber) {
+  let count = 99;
+  for (let i = 100; i <= checkNumber; i++) {  
+    let number = ""+i;
+    let gap_1 = Number(number[1]) - Number(number[0]);
+    let gap_2 = Number(number[2]) - Number(number[1]);
+    if (gap_2 == gap_1) count++;
+    // console.log(gap_1,gap_2);
+  }
+  return count;
+}
+
+// createNumber();
