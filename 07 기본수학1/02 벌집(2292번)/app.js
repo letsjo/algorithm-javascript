52a0665099ab1ca2acf702fbde2a7d2eb8adf584
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-20 23:18:34 +0900

    completed

--- 07 기본수학1/02 벌집(2292번)/app.js
+++ 07 기본수학1/02 벌집(2292번)/app.js
@@ -8,12 +8,14 @@
 
 let input = require("fs")
   .readFileSync(__dirname + "/input.txt", { encoding: "utf-8" })
-  .split("\n")[0].split(' ').map(Number);
-// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split("\n")[0].trim().split(' ').map(Number);
-let count = 0;
-let sum = 0;
-if(input[1]>=input[2])
-  console.log(-1);
-else{
-  console.log(Math.floor(input[0]/(input[2]-input[1]))+1);
+  .split("\n")[0];
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split("\n")[0];
+
+input = Number(input);
+let count = 1;
+let level = 1;
+while(count<input){
+  count += (level++)*6;
 }
+
+console.log(level);
