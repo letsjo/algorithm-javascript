25663649ed126b1c31f1845edcf16a34913b30c7
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-22 17:26:36 +0900

    completed

---
+++ 07 기본수학1/04 달팽이는 올라가고 싶다(2869번)/app.js
@@ -0,0 +1,18 @@
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n')[0].split(" ").map(Number);
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(" ").map(Number);
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n').map(Number);
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0];
+
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\r\n');
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\r\n');
+
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt", { encoding: "utf-8" })
+  .split("\n")[0].split(" ").map(Number);
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split("\n")[0].split(" ").map(Number);
+
+let climb = input[0];
+let slide = input[1];
+let height = input[2];
+
+console.log(Math.floor((height-climb)-1/(climb-slide)+2));
