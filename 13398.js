64a378886ebe1facfd81d35b49cd55a0a3a135da
letsjo <hyunoh.jo@gmail.com>
2023-08-31 21:31:32 +0900

    Feat: add 연속합 2

---
+++ 13398.js
@@ -0,0 +1,33 @@
+// 연속합 2
+// https://www.acmicpc.net/problem/13398
+
+let input = require('fs')
+  .readFileSync(__dirname + '/input.txt', {
+    encoding: 'utf-8',
+  })
+  .toString()
+  .trim()
+  .split('\r\n');
+// let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0]);
+const numbers = input[1].split(' ').map(Number);
+
+const dp = Array.from(Array(n), () => new Array(2).fill(0));
+// const dp = Array(n).fill([0, 0]);
+
+dp[0][0] = numbers[0];
+dp[0][1] = numbers[0];
+
+for (let i = 1; i < n; i++) {
+  dp[i][0] = Math.max(dp[i - 1][0] + numbers[i], numbers[i]);
+  dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + numbers[i]);
+}
+
+let answer = -Infinity;
+
+for (let i = 0; i < n; i++) {
+  answer = Math.max(answer, dp[i][0], dp[i][1]);
+}
+
+console.log(answer);
