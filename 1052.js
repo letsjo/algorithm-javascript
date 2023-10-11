c9510b686a70de8242499bf66586473fa40137f5
letsjo <hyunoh.jo@gmail.com>
2023-10-11 22:29:19 +0900

    Feat: add 물병 with 비트마스킹

--- 1052.js
+++ 1052.js
@@ -8,6 +8,28 @@ let input = require('fs')
   .split('\r\n');
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n');
 
+// let [N, K] = input[0].split(' ').map((v) => +v);
+// let answer = 0;
+
+// while (true) {
+//   let count = 0;
+//   let temp = N;
+//   while (temp > 0) {
+//     if (temp % 2 === 1) {
+//       count += 1;
+//     }
+//     temp = Math.floor(temp / 2);
+//   }
+//   if (count <= K) {
+//     break;
+//   }
+//   N += 1;
+//   answer += 1;
+// }
+
+// console.log(answer);
+
+// 비트마스킹으로 풀기
 let [N, K] = input[0].split(' ').map((v) => +v);
 let answer = 0;
 
@@ -15,10 +37,10 @@ while (true) {
   let count = 0;
   let temp = N;
   while (temp > 0) {
-    if (temp % 2 === 1) {
+    if (temp & 1) {
       count += 1;
     }
-    temp = Math.floor(temp / 2);
+    temp >>= 1;
   }
   if (count <= K) {
     break;
