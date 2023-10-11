10fd28efc856e604d1673b3d2e13ca78c860c266
letsjo <hyunoh.jo@gmail.com>
2023-10-11 22:28:55 +0900

    Feat: add 물병

---
+++ 1052.js
@@ -0,0 +1,30 @@
+// 물병
+// https://www.acmicpc.net/problem/1052
+
+let input = require('fs')
+  .readFileSync(__dirname + '/input.txt', {
+    encoding: 'utf-8',
+  })
+  .split('\r\n');
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n');
+
+let [N, K] = input[0].split(' ').map((v) => +v);
+let answer = 0;
+
+while (true) {
+  let count = 0;
+  let temp = N;
+  while (temp > 0) {
+    if (temp % 2 === 1) {
+      count += 1;
+    }
+    temp = Math.floor(temp / 2);
+  }
+  if (count <= K) {
+    break;
+  }
+  N += 1;
+  answer += 1;
+}
+
+console.log(answer);
