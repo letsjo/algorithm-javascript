c1d5650cb38d1df2e722b014b96b6db81e5aab9f
letsjo <hyunoh.jo@gmail.com>
2023-10-12 02:35:36 +0900

    Fix: change path baekjoon

--- 00 자료구조1/01 스택(10828번)/app.js
+++
@@ -1,4 +0,0 @@
-let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "utf-8" }).split("\n");
-// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split("\n");
-
-console.log(input);
