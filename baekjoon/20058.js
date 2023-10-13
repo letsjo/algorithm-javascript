fb31f7945aecc85ff846d2eb33a0a2aaed69a847
letsjo <hyunoh.jo@gmail.com>
2023-10-13 18:16:56 +0900

    Feat: add descreption comment to code

--- baekjoon/20058.js
+++ baekjoon/20058.js
@@ -11,35 +11,52 @@ let input = require('fs')
 // let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const [N, Q] = input[0].split(' ').map(Number);
-const map = input.slice(1).map((line) => line.split(' ').map(Number));
-const L = map.pop().map(Number);
+const iceMap = input.slice(1).map((line) => line.split(' ').map(Number));
+const L = iceMap.pop().map(Number);
 const mapSize = 2 ** N;
 
-const dx = [0, 1, 0, -1];
-const dy = [-1, 0, 1, 0];
-
+/**
+ * 아이스블록을 회전시키는 함수
+ * @param {number} x // 회전시킬 아이스블록의 x좌표
+ * @param {number} y // 회전시킬 아이스블록의 y좌표
+ * @param {number} l // 회전시킬 아이스블록의 크기
+ * @returns {void}
+ */
 const rotateIceBlock = (x, y, l) => {
   const temp = Array.from(Array(l), () => new Array(l).fill(0));
 
+  // 아이스블록을 90도 회전시키기
+  // l = 2
+  // temp[0][0] => iceMap[0+2-1-0][0+0] = iceMap[1][0];
+  // temp[0][1] => iceMap[0+2-1-1][0+0] = iceMap[0][0];
+  // temp[1][0] => iceMap[0+2-1-0][0+1] = iceMap[1][1];
+  // temp[1][1] => iceMap[0+2-1-1][0+1] = iceMap[0][1];
   for (let i = 0; i < l; i += 1) {
     for (let j = 0; j < l; j += 1) {
-      temp[i][j] = map[y + l - 1 - j][x + i];
+      temp[i][j] = iceMap[y + l - 1 - j][x + i];
     }
   }
 
   for (let i = 0; i < l; i += 1) {
     for (let j = 0; j < l; j += 1) {
-      map[y + i][x + j] = temp[i][j];
+      iceMap[y + i][x + j] = temp[i][j];
     }
   }
 };
 
+/**
+ * 얼음 녹이는 함수
+ * @returns {void}
+ */
 const meltIceBlock = () => {
   const temp = Array.from(Array(mapSize), () => new Array(mapSize).fill(0));
 
+  const dx = [0, 1, 0, -1];
+  const dy = [-1, 0, 1, 0];
+
   for (let i = 0; i < mapSize; i += 1) {
     for (let j = 0; j < mapSize; j += 1) {
-      if (map[i][j] === 0) continue;
+      if (iceMap[i][j] === 0) continue;
 
       let count = 0;
 
@@ -48,25 +65,34 @@ const meltIceBlock = () => {
         const ny = i + dy[k];
 
         if (nx < 0 || ny < 0 || nx >= mapSize || ny >= mapSize) continue;
-        if (map[ny][nx] > 0) count += 1;
+        if (iceMap[ny][nx] > 0) count += 1;
       }
 
-      if (count < 3) temp[i][j] = map[i][j] - 1;
-      else temp[i][j] = map[i][j];
+      if (count < 3) temp[i][j] = iceMap[i][j] - 1;
+      else temp[i][j] = iceMap[i][j];
     }
   }
 
   for (let i = 0; i < mapSize; i += 1) {
     for (let j = 0; j < mapSize; j += 1) {
-      map[i][j] = temp[i][j];
+      iceMap[i][j] = temp[i][j];
     }
   }
 };
 
+/**
+ * 연결된 아이스블록의 갯수를 세는 함수
+ * @param {*} x
+ * @param {*} y
+ * @returns {number} 연결된 아이스블록의 갯수
+ */
 const bfs = (x, y) => {
-  let count = 1;
-  const queue = [[x, y]];
   const visited = Array.from(Array(mapSize), () => new Array(mapSize).fill(false));
+  const dx = [0, 1, 0, -1];
+  const dy = [-1, 0, 1, 0];
+  const queue = [[x, y]];
+  let count = 1;
+
   visited[y][x] = true;
 
   while (queue.length > 0) {
@@ -77,7 +103,7 @@ const bfs = (x, y) => {
       const ny = cy + dy[i];
 
       if (nx < 0 || ny < 0 || nx >= mapSize || ny >= mapSize) continue;
-      if (visited[ny][nx] || map[ny][nx] === 0) continue;
+      if (visited[ny][nx] || iceMap[ny][nx] === 0) continue;
 
       visited[ny][nx] = true;
       queue.push([nx, ny]);
@@ -89,14 +115,17 @@ const bfs = (x, y) => {
 };
 
 for (let i = 0; i < Q; i += 1) {
+  // 회전시킬 아이스블록의 크기
   const l = 2 ** L[i];
 
   for (let j = 0; j < mapSize; j += l) {
     for (let k = 0; k < mapSize; k += l) {
-      rotateIceBlock(k, j, l);
+      rotateIceBlock(j, k, l);
     }
   }
   meltIceBlock();
+
+  console.log(iceMap);
 }
 
 let sum = 0;
@@ -105,9 +134,9 @@ const visited = Array.from(Array(mapSize), () => new Array(mapSize).fill(false))
 
 for (let i = 0; i < mapSize; i += 1) {
   for (let j = 0; j < mapSize; j += 1) {
-    sum += map[i][j];
+    sum += iceMap[i][j];
 
-    if (map[i][j] === 0 || visited[i][j]) continue;
+    if (iceMap[i][j] === 0 || visited[i][j]) continue;
 
     const count = bfs(j, i);
     max = Math.max(max, count);
